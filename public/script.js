e69a3341fc137f071a5658a0e381d06db97a1864
Tazmah <abdulmuizzayo6@gmail.com>
2022-11-20 03:16:13 +0700

    updated and added some cool stuffs

--- public/script.js
+++ public/script.js
@@ -1,7 +1,17 @@
 let nav = document.getElementById("toggle");
 let two = document.getElementById("two");
+let close = document.getElementById("close");
+// let bodyElement = document.getElementsByTagName("body");
 
 nav.addEventListener("click", () => {
+    document.body.style.overflowY = 'hidden';
+    two.classList.toggle("fixed");
+    two.classList.toggle("hidden");
+    two.classList.toggle("grid");
+});
+close.addEventListener("click", () => {
+    document.body.style.overflowY = 'scroll';
+    two.classList.toggle("fixed");
     two.classList.toggle("hidden");
     two.classList.toggle("grid");
 });
